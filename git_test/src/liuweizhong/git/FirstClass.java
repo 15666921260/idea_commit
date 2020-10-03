4c114481c7f7c2192ce40e2e821763a96ac95be8
admin <978959516@qq.com>
2020-10-03 15:00:43 +0800

    First git project update git_test;

--- git_test/src/liuweizhong/git/FirstClass.java
+++ git_test/src/liuweizhong/git/FirstClass.java
@@ -4,6 +4,11 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("我是Git集成的类");
+        test(22,63);
+    }
+
+    public static void test(int a,int b){
+        System.out.println(a+b);
     }
 
 }
